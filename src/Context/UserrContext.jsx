a943971714ccaf528f532f5009da79fc71d65f92
MostafaD <136201384+MostafaD5@users.noreply.github.com>
2025-05-01 19:24:15 +0300

    Updated README.md with live demo link

--- src/Context/UserrContext.jsx
+++ src/Context/UserrContext.jsx
@@ -1,20 +1,21 @@
 import { createContext, useState } from "react";
 
-export const UserrContext = createContext("");
+export const UserrContext = createContext(null);
+
 export default function UserProvider({ children }) {
-  let [token, settoken] = useState(localStorage.getItem("token"));
+  const [token, settoken] = useState(localStorage.getItem("token") || null);
   const [notesCount, setNotesCount] = useState(0);
+
   function logOut() {
     settoken(null);
     localStorage.removeItem("token");
   }
+
   return (
-    <>
-      <UserrContext.Provider
-        value={{ token, settoken, logOut, notesCount, setNotesCount }}
-      >
-        {children}
-      </UserrContext.Provider>
-    </>
+    <UserrContext.Provider
+      value={{ token, settoken, logOut, notesCount, setNotesCount }}
+    >
+      {children}
+    </UserrContext.Provider>
   );
 }
